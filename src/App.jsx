11671c168fd7c58006539ab3db42890d438f5093
GettingStarted.app <shayaan.siddiqui@gmail.com>
2023-11-28 15:52:00 -0500

    declaring the winner

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,12 @@ import {useState} from "react";
 import Log from "./component/Log.jsx";
 import {WINNING_COMBINATIONS} from "./winning-combinations.js";
 
+const initialGameBoard = [
+	[null, null, null],
+	[null, null, null],
+	[null, null, null]
+]
+
 const derivedActivePlayer = (gameTurns) => {
 	let currentPlayer = 'X';
 	if(gameTurns.length > 0 && gameTurns[0].player === 'X')
@@ -18,6 +24,26 @@ function App() {
 
 	const activePlayer = derivedActivePlayer(gameTurns);
 
+	let gameBoard = initialGameBoard;
+	for(const turn of gameTurns) {
+		const { square, player } = turn;
+		const { row, column } = square;
+		gameBoard[row][column] = player;
+	}
+
+	let winner = null;
+
+	for(const combination of WINNING_COMBINATIONS) {
+		const firstSquareSymbol = gameBoard[combination[0].row][combination[0].column];
+		const secondSquareSymbol = gameBoard[combination[1].row][combination[1].column];
+		const thirdSquareSymbol = gameBoard[combination[2].row][combination[2].column];
+
+		if(firstSquareSymbol && firstSquareSymbol === secondSquareSymbol && firstSquareSymbol === thirdSquareSymbol)
+		{
+			// Won game
+			winner = firstSquareSymbol;
+		}
+	}
 	const handleSelectSquare = (rowIndex, columnIndex) => {
 		setGameTurns(prevTurns => {
 			let currentPlayer = derivedActivePlayer(gameTurns);
@@ -34,7 +60,8 @@ function App() {
 					<Player initialName={'Player 1'} symbol={'X'} isActive={activePlayer === 'X'}/>
 					<Player initialName={'Player 2'} symbol={'O'} isActive={activePlayer === 'O'}/>
 				</ol>
-				<GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns}/>
+				{winner && <p>You won, {winner}!</p>}
+				<GameBoard onSelectSquare={handleSelectSquare} board={gameBoard}/>
 			</div>
 			<Log turns={gameTurns}/>
 		</main>
--- src/component/GameBoard.jsx
+++ src/component/GameBoard.jsx
@@ -1,20 +1,9 @@
-const initialGameBoard = [
-	[null, null, null],
-	[null, null, null],
-	[null, null, null]
-]
-const GameBoard = ({onSelectSquare, turns}) => {
-	let gameBoard = initialGameBoard;
-	for(const turn of turns) {
-		const { square, player } = turn;
-		const { row, column } = square;
-		gameBoard[row][column] = player;
-	}
 
+const GameBoard = ({onSelectSquare, board}) => {
 	return (
 		<ol id={'game-board'}>
 			{
-				gameBoard.map((row, rowIndex) =>
+				board.map((row, rowIndex) =>
 					<li key={rowIndex}>
 						<ol>
 							{
